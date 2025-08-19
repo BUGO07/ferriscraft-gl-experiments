7298ab169cbcf03c48f980804c5f4b7ad8ce0da7
BUGO07 <71205861+BUGO07@users.noreply.github.com>
2025-08-19 18:06:17 +0400

    more abstractions

--- src/events.rs
+++ src/events.rs
@@ -5,7 +5,7 @@ use crate::ecs::*;
 #[derive(Event)]
 pub struct WindowEventECS(pub WindowEvent);
 
-pub fn handle_input(mut keyboard: ResMut<KeyboardInput>, mut mouse: ResMut<MouseInput>) {
+pub fn handle_input_cleanup(mut keyboard: ResMut<KeyboardInput>, mut mouse: ResMut<MouseInput>) {
     keyboard.just_pressesd.clear();
     keyboard.just_released.clear();
 
@@ -15,32 +15,33 @@ pub fn handle_input(mut keyboard: ResMut<KeyboardInput>, mut mouse: ResMut<Mouse
     mouse.scroll = Vec2::ZERO;
 }
 
-pub fn handle_window(ns_window: NonSendMut<NSWindow>, window: Res<Window>, mut rerun: Local<bool>) {
-    if !*rerun {
+pub fn handle_window(
+    ns_window: NonSendMut<NSWindow>,
+    mut window: ResMut<Window>,
+    mut not_first_run: Local<bool>,
+) {
+    if !*not_first_run {
         // don't do anything on the first frame
         // changing cursor visibility doesn't work on the first frame
-        *rerun = true;
+        *not_first_run = true;
         return;
     }
     match window.cursor_grab {
         CursorGrabMode::None => {
             ns_window
-                .winit_window
+                .winit
                 .set_cursor_grab(CursorGrabMode::None)
                 .unwrap();
         }
         _ => ns_window
-            .winit_window
+            .winit
             .set_cursor_grab(CursorGrabMode::Locked)
-            .or_else(|_| {
-                ns_window
-                    .winit_window
-                    .set_cursor_grab(CursorGrabMode::Confined)
-            })
+            .or_else(|_| ns_window.winit.set_cursor_grab(CursorGrabMode::Confined))
             .unwrap(),
     }
 
-    ns_window
-        .winit_window
-        .set_cursor_visible(window.cursor_visible);
+    ns_window.winit.set_cursor_visible(window.cursor_visible);
+
+    window.height = ns_window.winit.inner_size().height;
+    window.width = ns_window.winit.inner_size().width;
 }
--- src/render.rs
+++ src/render.rs
@@ -1,31 +1,24 @@
-use std::{
-    collections::HashMap,
-    f32::consts::{FRAC_PI_4, FRAC_PI_6},
-};
+use std::collections::HashMap;
 
 use egui_glium::EguiGlium;
 use glium::{
-    BackfaceCullingMode, Depth, DepthTest, DrawParameters, IndexBuffer, Program, Surface,
-    Texture2d, VertexBuffer,
+    BackfaceCullingMode, Depth, DepthTest, DrawParameters, IndexBuffer, Surface, VertexBuffer,
     index::PrimitiveType,
-    texture::RawImage2d,
     uniforms::{MagnifySamplerFilter, MinifySamplerFilter},
 };
-use image::ImageFormat;
 
 use crate::{
-    CHUNK_SIZE, SEA_LEVEL, UI_FRAGMENT_SHADER, UI_VERTEX_SHADER, VOXEL_FRAGMENT_SHADER,
-    VOXEL_VERTEX_SHADER,
+    CHUNK_SIZE, SEA_LEVEL,
     ecs::*,
     mesher::{Chunk, ChunkMesh, Direction, UIVertex},
     utils::{Quad, frustum_planes, generate_block_at, should_cull, vec3_to_index},
 };
 
-pub fn render_setup(
+pub fn setup(
     mut commands: Commands,
-    window: NonSend<NSWindow>,
     mut meshes: NonSendMut<Meshes>,
     mut materials: NonSendMut<Materials>,
+    window: NonSend<NSWindow>,
 ) {
     commands.spawn((
         Camera3d {
@@ -41,16 +34,20 @@ pub fn render_setup(
         DirectionalLight {
             illuminance: 1000.0,
         },
-        Transform::DEFAULT
-            .with_rotation(Quat::from_rotation_x(FRAC_PI_4) * Quat::from_rotation_y(-FRAC_PI_6)),
+        Transform::DEFAULT.with_rotation(
+            Quat::from_rotation_x(45_f32.to_radians())
+                * Quat::from_rotation_y(-30_f32.to_radians()),
+        ),
     ));
+
+    let ui_material = materials.add(Material::new(&window.facade, "ui", None));
 
     commands.spawn(UIRect::new(
         Val::Percent(0.0),
         Val::Percent(0.0),
         Val::Px(80.0),
         Val::Px(80.0),
-        Vec4::ONE,
+        ui_material,
     ));
 
     commands.spawn(UIRect::new(
@@ -58,28 +55,10 @@ pub fn render_setup(
         Val::Percent(50.0),
         Val::Percent(1.0),
         Val::Percent(1.0),
-        Vec4::ONE,
+        ui_material,
     ));
 
-    let image = image::load(
-        std::io::Cursor::new(std::fs::read("assets/atlas.png").unwrap()),
-        ImageFormat::Png,
-    )
-    .unwrap()
-    .to_rgba8();
-    let image_dimensions = image.dimensions();
-    let image = RawImage2d::from_raw_rgba_reversed(&image.into_raw(), image_dimensions);
-    let texture = Texture2d::new(&window.gl_context, image).unwrap();
-    let material = materials.add(Material::new(
-        Program::from_source(
-            &window.gl_context,
-            VOXEL_VERTEX_SHADER,
-            VOXEL_FRAGMENT_SHADER,
-            None,
-        )
-        .unwrap(),
-        texture,
-    ));
+    let voxel_material = materials.add(Material::new(&window.facade, "voxel", Some("atlas.png")));
 
     const CHUNK_SIZE_VEC: Vec3 = Vec3::splat(CHUNK_SIZE as f32);
 
@@ -110,9 +89,9 @@ pub fn render_setup(
 
     for (chunk_pos, chunk) in &chunks {
         if let Some(mesh_data) = ChunkMesh::default().build(chunk, &chunks) {
-            let vertex_buffer = VertexBuffer::new(&window.gl_context, &mesh_data.vertices).unwrap();
+            let vertex_buffer = VertexBuffer::new(&window.facade, &mesh_data.vertices).unwrap();
             let index_buffer = IndexBuffer::new(
-                &window.gl_context,
+                &window.facade,
                 PrimitiveType::TrianglesList,
                 &mesh_data.indices,
             )
@@ -122,7 +101,7 @@ pub fn render_setup(
 
             commands.spawn((
                 mesh_id,
-                material,
+                voxel_material,
                 Transform::from_translation((chunk_pos * CHUNK_SIZE).as_vec3()),
                 Aabb::new(Vec3::ZERO, CHUNK_SIZE_VEC),
             ));
@@ -145,76 +124,79 @@ pub fn render_update(
     debug_info: Option<ResMut<DebugInfo>>,
     mut egui: NonSendMut<EguiGlium>,
 ) {
-    let mut target = window.gl_context.draw();
+    let mut target = window.facade.draw();
     target.clear_color_and_depth((0.0, 0.0, 1.0, 1.0), 1.0);
-
-    let (camera_transform, camera) = camera_query.into_inner();
-    let (light_transform, light) = light_query.into_inner();
-
     let (width, height) = target.get_dimensions();
-    let perspective = Mat4::perspective_rh_gl(
-        camera.fov.to_radians(),
-        width as f32 / height as f32,
-        camera.near,
-        camera.far,
-    );
-
-    let view = camera_transform.as_mat4().inverse();
-    let vp: Mat4 = perspective * view;
-    let frustum = frustum_planes(&vp);
 
     let mut draw_calls = 0;
     let mut vertices = 0;
     let mut indices = 0;
 
-    for (chunk_transform, mesh_id, material_id, aabb) in mesh_entities.iter() {
-        if should_cull(&frustum, chunk_transform.translation, aabb) {
-            continue;
-        }
-        let Mesh {
-            vertex_buffer,
-            index_buffer,
-        } = &meshes.0[mesh_id.0];
-        let Material { program, texture } = &materials.0[material_id.0];
+    // chunks
+    {
+        let (camera_transform, camera) = camera_query.into_inner();
+        let (light_transform, light) = light_query.into_inner();
 
-        let sampler = texture
-            .sampled()
-            .magnify_filter(MagnifySamplerFilter::Nearest)
-            .minify_filter(MinifySamplerFilter::NearestMipmapNearest);
+        let perspective = Mat4::perspective_rh_gl(
+            camera.fov.to_radians(),
+            width as f32 / height as f32,
+            camera.near,
+            camera.far,
+        );
 
-        let uniforms = uniform! {
-            model: chunk_transform.as_mat4().to_cols_array_2d(),
-            view: view.to_cols_array_2d(),
-            perspective: perspective.to_cols_array_2d(),
-            tex: sampler,
-            u_light: (view * Mat4::from_quat(light_transform.rotation) * Vec4::NEG_Z).truncate().normalize().extend(light.illuminance).to_array(),
-        };
+        let view = camera_transform.as_mat4().inverse();
+        let vp: Mat4 = perspective * view;
+        let frustum = frustum_planes(&vp);
 
-        let params = DrawParameters {
-            depth: Depth {
-                test: DepthTest::IfLess,
-                write: true,
+        for (chunk_transform, mesh_id, material_id, aabb) in mesh_entities.iter() {
+            if should_cull(&frustum, chunk_transform.translation, aabb) {
+                continue;
+            }
+            let Mesh {
+                vertex_buffer,
+                index_buffer,
+            } = &meshes.0[mesh_id.0];
+            let Material { program, texture } = &materials.0[material_id.0];
+
+            let sampler = texture
+                .sampled()
+                .magnify_filter(MagnifySamplerFilter::Nearest)
+                .minify_filter(MinifySamplerFilter::NearestMipmapNearest);
+
+            let uniforms = uniform! {
+                model: chunk_transform.as_mat4().to_cols_array_2d(),
+                view: view.to_cols_array_2d(),
+                perspective: perspective.to_cols_array_2d(),
+                tex: sampler,
+                u_light: (view * Mat4::from_quat(light_transform.rotation) * Vec4::NEG_Z).truncate().normalize().extend(light.illuminance).to_array(),
+            };
+
+            let params = DrawParameters {
+                depth: Depth {
+                    test: DepthTest::IfLess,
+                    write: true,
+                    ..Default::default()
+                },
+                backface_culling: BackfaceCullingMode::CullClockwise,
                 ..Default::default()
-            },
-            backface_culling: BackfaceCullingMode::CullClockwise,
-            ..Default::default()
-        };
+            };
 
-        vertices += vertex_buffer.len();
-        indices += index_buffer.len();
-        draw_calls += 1;
+            vertices += vertex_buffer.len();
+            indices += index_buffer.len();
+            draw_calls += 1;
 
-        target
-            .draw(vertex_buffer, index_buffer, program, &uniforms, &params)
-            .unwrap();
+            target
+                .draw(vertex_buffer, index_buffer, program, &uniforms, &params)
+                .unwrap();
+        }
     }
+
+    // TODO shadow mapping
+    {}
 
     // ui
     {
         let window_size = vec2(width as f32, height as f32);
-
-        let mut verts = Vec::new();
-        let mut inds = Vec::new();
 
         for ui_item in ui_query.iter() {
             let quad = Quad::from_direction(
@@ -230,37 +212,48 @@ pub fn render_update(
                     0.0,
                 ),
             );
-            verts.extend(quad.corners.iter().map(|c| UIVertex { pos: [c[0], c[1]] }));
+            let verts = quad
+                .corners
+                .iter()
+                .map(|c| UIVertex { pos: [c[0], c[1]] })
+                .collect::<Vec<_>>();
+
+            let inds = (0..verts.len() / 4)
+                .flat_map(|i| {
+                    let idx = i as u32 * 4;
+                    [idx, idx + 1, idx + 2, idx, idx + 2, idx + 3]
+                })
+                .collect::<Vec<_>>();
+
+            let vertex_buffer = VertexBuffer::new(&window.facade, &verts).unwrap();
+            let index_buffer =
+                IndexBuffer::new(&window.facade, PrimitiveType::TrianglesList, &inds).unwrap();
+
+            vertices += vertex_buffer.len();
+            indices += index_buffer.len();
+            draw_calls += 1;
+
+            let Material { program, texture } = &materials.0[ui_item.material.0];
+
+            let sampler = texture
+                .sampled()
+                .magnify_filter(MagnifySamplerFilter::Nearest)
+                .minify_filter(MinifySamplerFilter::NearestMipmapNearest);
+
+            let uniforms = uniform! {
+                tex: sampler,
+            };
+
+            target
+                .draw(
+                    &vertex_buffer,
+                    &index_buffer,
+                    program,
+                    &uniforms,
+                    &DrawParameters::default(),
+                )
+                .unwrap();
         }
-
-        verts.shrink_to_fit();
-        inds.extend((0..verts.len() / 4).flat_map(|i| {
-            let idx = i as u32 * 4;
-            [idx, idx + 1, idx + 2, idx, idx + 2, idx + 3]
-        }));
-
-        let vertex_buffer = VertexBuffer::new(&window.gl_context, &verts).unwrap();
-        let index_buffer =
-            IndexBuffer::new(&window.gl_context, PrimitiveType::TrianglesList, &inds).unwrap();
-        let program = Program::from_source(
-            &window.gl_context,
-            UI_VERTEX_SHADER,
-            UI_FRAGMENT_SHADER,
-            None,
-        )
-        .unwrap();
-
-        let uniforms = uniform! {};
-
-        let params = DrawParameters::default();
-
-        vertices += vertex_buffer.len();
-        indices += index_buffer.len();
-        draw_calls += 1;
-
-        target
-            .draw(&vertex_buffer, &index_buffer, &program, &uniforms, &params)
-            .unwrap();
     }
 
     if let Some(mut debug_info) = debug_info {
@@ -268,6 +261,6 @@ pub fn render_update(
         debug_info.indices = indices;
         debug_info.draw_calls = draw_calls;
     }
-    egui.paint(&window.gl_context, &mut target);
+    egui.paint(&window.facade, &mut target);
     target.finish().unwrap();
 }
